5d915861f12c881057c34224368dfdfe980d2e49
yfwangsh <yfwangsh@citiz.net>
2020-03-24 17:11:33 +0800

    add utils

---
+++ lib/utils.py
@@ -0,0 +1,16 @@
+from time import clock
+ 
+class Timer(object):
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+ 
+    def __enter__(self):
+        self.start = clock()
+        return self
+ 
+    def __exit__(self, *args):
+        self.end = clock()
+        self.secs = self.end - self.start
+        self.msecs = self.secs * 1000  # millisecs
+        if self.verbose:
+            print('elapsed time: %f ms' %(self.msecs))
